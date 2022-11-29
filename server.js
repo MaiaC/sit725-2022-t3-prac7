f8fc82676858f21b1e337b670a43d7083c04bc0a
Maia Coghlan <34679505+MaiaC@users.noreply.github.com>
2022-11-29 18:02:45 +1100

    connect to mongodb

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 var express = require("express");
 var app = express();
+var cors = require("cors");
 var port = process.env.port || 3000;
+let projectCollection;
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+// mongo db connection
+const MongoClient = require("mongodb").MongoClient;
+
+// add database connections
+const uri =
+  "mongodb+srv://maiaccog:prac4@cluster0.sbhkstm.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+const createCollection = (collectionName) => {
+  client.connect((err, db) => {
+    projectCollection = client.db().collection(collectionName);
+    if (!err) {
+      console.log("MongoDB connected");
+    } else {
+      console.log("db error: ", err);
+      process.exit(1);
+    }
+  });
+};
 
 const cardList = [
   {
@@ -27,4 +50,5 @@ app.get("/api/projects", (req, res) => {
 
 app.listen(port, () => {
   console.log("App listening to: http://localhost:" + port);
+  createCollection("Destinations");
 });
